6dd280243b3518be6bdcc908795ba65dfe3b8a4e
jason-nordheim <jason.nordheim@gmail.com>
2020-04-28 12:14:12 -0600

    added ability to get activities via api

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,11 @@
 class ActivitiesController < ApplicationController
+  def index 
+    @acitivities = Activity.all 
+    render json: @acitivities
+  end 
+
+  def show 
+    @activity = Activity.find(params[:id])
+    render json: @activity 
+  end 
 end
